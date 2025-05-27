5b16412fcbb82e68ce58ea5561ae6d00230c75c0
wubenson0419 <126497777+wubenson0419@users.noreply.github.com>
2025-05-27 15:27:54 +0800

    创建 sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'clock-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
